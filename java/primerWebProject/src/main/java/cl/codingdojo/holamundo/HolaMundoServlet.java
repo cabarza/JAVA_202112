9330ee21b72df9a71dd760d2132e71b9636cd5c6
Cesar Abarza <cesar.abarza@gmail.com>
2021-12-22 15:18:43 -0300

    Se crea ejemplo de proyecto web con servlets en java

---
+++ java/primerWebProject/src/main/java/cl/codingdojo/holamundo/HolaMundoServlet.java
@@ -0,0 +1,45 @@
+package cl.codingdojo.holamundo;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HolaMundoServlet
+ */
+@WebServlet(urlPatterns = "/hola2", description = "Este es mi primer Servlet", displayName = "HolaMundoServlet")
+public class HolaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HolaMundoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        // writes the response
+        PrintWriter out = response.getWriter();
+        out.write("<h1>Hola Mundo!!!</h1>");
+	}
+
+}
